ded4781b3ad62af262b28c6df2c283357b4c8330
karim <y2793623b@iespoblenou.org>
2017-05-12 17:29:43 +0200

    map + overlays + buttones (foto / video) connectar a la firebase utilizando el correo personal (no del institut) + activar la camera

--- app/src/main/java/com/example/y2793623b/map_fotos_pro/MainActivityFragment.java
+++ app/src/main/java/com/example/y2793623b/map_fotos_pro/MainActivityFragment.java
@@ -1,11 +1,17 @@
 package com.example.y2793623b.map_fotos_pro;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
@@ -16,6 +22,11 @@ import org.osmdroid.views.overlay.compass.CompassOverlay;
 import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
 import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -29,10 +40,10 @@ public class MainActivityFragment extends Fragment {
     private CompassOverlay mCompassOverlay;
     //private MinimapOverlay mMinimapOverlay;
 
+
+    private Button takeFoto;
     public MainActivityFragment() {
     }
-
-    //private MapView
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,12 +52,20 @@ public class MainActivityFragment extends Fragment {
 
 
         map = (MapView) view.findViewById(R.id.mapView);
+        takeFoto = (Button) view.findViewById(R.id.bt_foto);
 
         initializeMap();
         setZoom();
         setOverlays();
 
         map.invalidate();
+
+        takeFoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
 
         return view;
     }
@@ -108,5 +127,57 @@ public class MainActivityFragment extends Fragment {
         map.getOverlays().add(this.mCompassOverlay);
 
 
+    }
+
+    //Fem La Foto
+    static final int REQUEST_TAKE_PHOTO = 1;
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            photoFile = createImageFile();
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
+                        Uri.fromFile(photoFile));
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+                Log.d("photo ------ > : " , photoFile.toString());
+            }
+        }
+    }
+
+    //Creem un fitxer on guardar la foto
+    String mCurrentPhotoPath;
+    private static final int ACTIVITAT_SELECCIONAR_IMATGE = 1;
+    private File createImageFile() {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES);
+        File image = null;
+        try {
+            image = File.createTempFile(
+                    imageFileName,  // prefix
+                    ".jpg",         // suffix
+                    storageDir      // directory
+            );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        Log.d(" photo path  -------------------- : " , mCurrentPhotoPath );
+
+        Intent i = new Intent(
+                Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        );
+        startActivityForResult(i, ACTIVITAT_SELECCIONAR_IMATGE);
+
+
+
+        return image;
     }
 }
